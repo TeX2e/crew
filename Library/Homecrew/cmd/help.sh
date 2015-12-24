fff77e8402d7dcd3641412d31eae92c7b73b0bae
Mako <mnfeconicu41@gmail.com>
2015-12-24 18:19:28 +0900

    update crew-help

--- Library/Homecrew/cmd/help.sh
+++ Library/Homecrew/cmd/help.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
 function crew-help {
-  echo "crew: install tool for Cygwin"
+  echo "Overview:"
+  echo "  crew install tool for Cygwin similar to brew"
+  echo
+  echo "Example usage:"
+  echo "  crew info <packages>"
+  echo "  crew install <packages>"
+  echo "  crew uninstall <packages>"
+  echo "  crew search <text_or_regex>"
+  echo "  crew list [<packages>]"
+  echo "  crew update"
+  echo "  crew upgrade [<packages>]"
+  echo "  crew pin/unpin <packages>"
+  echo
+  echo "Troubleshooting:"
+  echo "  crew doctor"
 }
