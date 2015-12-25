380f980bd5b4f39621ef9be9284ccb061b69bc93
Mako <mnfeconicu41@gmail.com>
2015-12-25 20:42:50 +0900

    implement install.sh

--- Library/Homecrew/cmd/install.sh
+++ Library/Homecrew/cmd/install.sh
@@ -5,39 +5,53 @@ function crew-install { # <packages>
   local pkg
   for pkg in $pkgs
   do
-    download $pkg
+    mkdir -p "$CREW_FORMULA/$pkg"
+    download $pkg | tee /tmp/crew-download
+
+    # pkg.tar.xz must be deployed
+
+    local download_file=$(cat /tmp/crew-download | awk '/Download/ {print $3}')
+    local pkg_version=${download_file%.tar.xz}
+    local working_dir="$CREW_FORMULA/$pkg/$pkg_version"
+    cd "$working_dir"
+    mkdir -p "$CREW_CELLER/$pkg/$pkg_version"
+
+    # decompress
+    xz -cdv "$pkg_version.tar.xz" > "$pkg_version.tar"
+    tar -x -C "$CREW_CELLER/$pkg/$pkg_version" -f "$pkg_version.tar"
+    tar tf "$pkg_version.tar.xz" | gzip > "$pkg_version.lst.gz"
+
+    rm "$pkg_version.tar"
   done
-  # pkg.tar.xz must be deployed
 }
 
 
 function download { # <package>
   local pkg=$1
   local cache_dir=$CREW_CACHE
-  local arch_dir=$(uname -m)
   local mirror=$(crew-mirror | sed -e 's,/$,,')
 
   if (( ! $# )); then
     error "no packages given"
   fi
 
-  if ! [[ -e "$cache_dir/$arch_dir/setup.ini" ]]; then
-    error "setup.ini is not found in $cache_dir/$arch_dir/"
+  if ! [[ -e "$cache_dir/setup.ini" ]]; then
+    error "setup.ini is not found in $cache_dir/"
   fi
   
   # look for package and save desc file
 
-  awk '$1 == pc' RS='\n\n@ ' FS='\n' pc="$pkg" "$cache_dir/$arch_dir/setup.ini" \
-    > "$cache_dir/$arch_dir/desc"
+  awk '$1 == pc' RS='\n\n@ ' FS='\n' pc="$pkg" "$cache_dir/setup.ini" \
+    > "$cache_dir/desc"
 
-  if [ ! -s "$cache_dir/$arch_dir/desc" ]; then
+  if [ ! -s "$cache_dir/desc" ]; then
     error "Unable to locate package $pkg"
   fi
 
   # download and unpack the bz2 or xz file
 
   # pick the latest version, which comes first
-  set -- $(awk '$1 == "install:"' "$cache_dir/$arch_dir/desc")
+  set -- $(awk '$1 == "install:"' "$cache_dir/desc")
 
   if (( ! $# )); then
     error 'Could not find "install" in package description: obsolete package?'
@@ -58,20 +72,16 @@ function download { # <package>
 
   if ! [ -e "$download_file" ] || ! $hash -c <<< "$digest $download_file" &>/dev/null; then
     info "fetch $mirror/$download_dir/$download_file"
-    wget -O "$download_file" "$mirror/$download_dir/$download_file"
+    wget -O "$download_file" "$mirror/$download_dir/$download_file" | info
     $hash -c <<< "$digest $download_file" &>/dev/null || exit
   fi
 
-  # decompress
-  # xz -cdv "${download_file}" > "${download_file%.xz}"
-  # tar -xf "${download_file%.xz}"
-  # local download_file_with_version=${download_file%.tar.xz}
-  # mkdir -p "$CREW_FORMULA/$pkg"
-  # tar tf "$download_file" | gzip > "$CREW_FORMULA/$pkg/$download_file_with_version.lst.gz"
-
   success "Download ${download_file}"
-  # return value
-  echo "download to: $cache_dir/$download_dir/$download_file"
+
+  # download file is put on Formula
+  local pkg_version="${download_file%.tar.xz}"
+  mkdir -p "$CREW_FORMULA/$pkg/$pkg_version"
+  mv "$cache_dir/$download_dir/$download_file" "$CREW_FORMULA/$pkg/$pkg_version/$download_file"
 }
 
 
