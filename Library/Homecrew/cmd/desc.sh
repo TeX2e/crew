4c088d86dd83fe756a5011c08b9c47dc1590bae1
Mako <mnfeconicu41@gmail.com>
2015-12-26 17:13:33 +0900

    implement desc.sh

--- Library/Homecrew/cmd/desc.sh
+++ Library/Homecrew/cmd/desc.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
 function crew-desc { # <packages>
-  :
+  check-packages "$@" || exit
+  local pkgs=$@
+  local pkg
+  for pkg in $pkgs
+  do
+    awk '
+      $1 == "@" && $2 == pkg {
+        getline
+        sub(/^sdesc: \"/, "", $0)
+        sub(/\"$/, "", $0)
+        print
+        exit
+      }
+      ' pkg="$pkg" "$CREW_CACHE/setup.ini"
+  done
 }
