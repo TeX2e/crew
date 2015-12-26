049b0fc98631f3684d5c074aee48976e346a0748
Mako <mnfeconicu41@gmail.com>
2015-12-26 13:02:06 +0900

    create config.sh

---
+++ Library/Homecrew/cmd/config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+function crew-config { # <packages>
+  echo "HOMECREW_PREFIX: $CREW_DIR"
+  echo "HOMECREW_CELLER: $CREW_CELLER"
+  echo "HOMECREW_FORMULA: $CREW_FORMULA"
+  echo "HOMECREW_CACHE: $CREW_CACHE"
+  which git &>/dev/null && echo "Git: $(which git)"
+}
