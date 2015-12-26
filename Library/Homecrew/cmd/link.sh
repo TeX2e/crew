6d2c3e34b044994ff05d4886411a2c812944754b
Mako <mnfeconicu41@gmail.com>
2015-12-26 11:25:29 +0900

    implement crew-link

--- Library/Homecrew/cmd/link.sh
+++ Library/Homecrew/cmd/link.sh
@@ -1,5 +1,50 @@
 #!/bin/bash
 
 function crew-link { # <packages>
-  :
+  local pkgs=$@
+  local pkg
+  for pkg in $pkgs
+  do
+    create-sym-link $pkg
+  done
+}
+
+function create-sym-link { # <package>[/version]
+  echo "$1" > /tmp/crew-sym-link
+  set -- $(cat /tmp/crew-sym-link | awk 'BEGIN { FS="/" } { print $1, $2 }')
+  local pkg=$1
+  local pkg_version=$2
+
+  if [[ ! -d "$CREW_FORMULA/$pkg" ]]; then
+    warn "no such a dir: $CREW_FORMULA/$pkg"
+    error "Package $pkg is not installed"
+  fi
+
+  # if it isn't passed the argument <version>, find latest version.
+  if [[ "$pkg_version" == "" ]]; then
+    pkg_version=$(cd $CREW_FORMULA/$pkg && /bin/ls | sort | tail -n 1)
+  fi
+
+  if [[ ! -d "$CREW_FORMULA/$pkg/$pkg_version" ]]; then
+    warn "no such a version: $pkg_version"
+    error "Package version missing, cannot remove $pkg"
+  fi
+
+  local pkg_formula="$CREW_FORMULA/$pkg/$pkg_version"
+
+  if [[ ! -f "$pkg_formula/$pkg_version.lst" ]]; then
+    warn "no such a list file: $pkg_formula/$pkg_version.lst"
+    error "Package manifest missing, cannot remove $pkg"
+  fi
+
+  cat "$pkg_formula/$pkg_version.lst" |\
+  awk '
+    /\/$/ {
+      print "mkdir -p \"" root "/" $0 "\""
+    }
+    /[^\/]$/ {
+      print "ln -sf \"" crew_celler "/" $0 "\"", "\"" root "/" $0 "\""
+    }
+    ' root="$ROOT_DIR" crew_celler="$CREW_CELLER" |\
+  sh
 }
