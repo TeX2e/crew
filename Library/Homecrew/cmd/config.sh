d9b132bdc0822d10e0fb14b94c96e201715072ce
Mako <mnfeconicu41@gmail.com>
2015-12-26 16:08:35 +0900

    fix typo

--- Library/Homecrew/cmd/config.sh
+++ Library/Homecrew/cmd/config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-function crew-config { # <packages>
+function crew-config {
   echo "HOMECREW_PREFIX: $CREW_DIR"
   echo "HOMECREW_CELLER: $CREW_CELLER"
   echo "HOMECREW_FORMULA: $CREW_FORMULA"
