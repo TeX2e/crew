6849a376f30928c53c450f32c6efb2512ca07e06
Mako <mnfeconicu41@gmail.com>
2015-12-27 09:34:03 +0900

    add mirror-server info to config.sh

--- Library/Homecrew/cmd/config.sh
+++ Library/Homecrew/cmd/config.sh
@@ -5,5 +5,6 @@ function crew-config {
   echo "HOMECREW_CELLER: $CREW_CELLER"
   echo "HOMECREW_FORMULA: $CREW_FORMULA"
   echo "HOMECREW_CACHE: $CREW_CACHE"
+  echo "mirror: $(crew-mirror)"
   which git &>/dev/null && echo "Git: $(which git)"
 }
