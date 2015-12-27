1ff78886ea346feb8b58b7933c86b37409c368ea
Mako <mnfeconicu41@gmail.com>
2015-12-27 19:03:33 +0900

    crew-update can take diff between setup.ini and setup.ini-save

--- Library/Homecrew/cmd/update.sh
+++ Library/Homecrew/cmd/update.sh
@@ -2,5 +2,26 @@
 
 function crew-update {
   update-setup-file
+
   # take diff
+  diff --speed-large-files \
+    <(
+      grep '^@\|^version:' $CREW_CACHE/setup.ini-save |
+      awk 'BEGIN { RS = "\n@ " } { print $1, $3, "\n" }'
+    ) \
+    <(
+      grep '^@\|^version:' $CREW_CACHE/setup.ini |
+      awk 'BEGIN { RS = "\n@ " } { print $1, $3, "\n" }'
+    ) |
+  awk '
+    BEGIN {
+      print "==> Updated Formulae"
+    }
+    /^</ {
+      printf "%s %s -> ", $2, $3
+    }
+    /^>/ {
+      print $3
+    }
+    '
 }
